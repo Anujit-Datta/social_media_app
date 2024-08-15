6610626f02dee9f08f735ce76ac0165d0e5f808b
Anujit Datta <anujit@Anujits-MacBook-Pro.local>
2024-08-15 09:54:18 -0700

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SnapShare extends StatelessWidget {
+  const SnapShare({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'SnapShare',
+      //home: ,
+    );
+  }
+}
